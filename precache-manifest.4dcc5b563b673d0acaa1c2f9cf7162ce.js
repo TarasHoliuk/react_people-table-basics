056c2b786c0948e7b192df14e30936f9e7044682
Taras Holiuk <taras.holiuk@gmail.com"dsveagj 3rm>
2021-09-19 21:22:29 +0300

    Updates

---
+++ precache-manifest.4dcc5b563b673d0acaa1c2f9cf7162ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3c39205b0b40ce47a16fd2bb4ce63d1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a2dc7bce5f83d789d8b0",
+    "url": "./static/css/2.93d2a445.chunk.css"
+  },
+  {
+    "revision": "ceb3467b9bdb550c352d",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "a2dc7bce5f83d789d8b0",
+    "url": "./static/js/2.56ee6212.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.56ee6212.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceb3467b9bdb550c352d",
+    "url": "./static/js/main.d4153622.chunk.js"
+  },
+  {
+    "revision": "c7ae2510e9f54903a2ed",
+    "url": "./static/js/runtime-main.b51b247a.js"
+  }
+]);
